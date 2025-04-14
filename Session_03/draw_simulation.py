9ff0daccd05f2c6bd930bde1d426520be93b4804
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-04-13 21:41:10 -0700

    add draw simulation

--- Session_03/draw_simulation.py
+++ Session_03/draw_simulation.py
@@ -1,3 +1,15 @@
+import random
+
+
+def trial():
+    balls = ["R", "R", "R", "G", "G", "G"]
+    result = ""
+    for _ in range(3):
+        draw = random.choice(balls)
+        result += draw
+    return result
+
+
 def simulate_draws(num_trials=1000):
     """
     Simulates drawing 3 balls with replacement from a bucket containing
@@ -19,3 +31,21 @@ def simulate_draws(num_trials=1000):
               Keys: "RRR", "RGR", "2R1G", "R>=G"
               Values: Integer counts of how many times each condition occurred.
     """
+
+    tracker = {}
+    for i in range(num_trials):
+        outcome = trial()
+        red = outcome.count("R")
+        green = outcome.count("G")
+        if outcome == "RRR":
+            tracker["RRR"] = tracker.get("RRR", 0) + 1
+        if outcome == "RGR":
+            tracker["RGR"] = tracker.get("RGR", 0) + 1
+        if red == 2 and green == 1:
+            tracker["2R1G"] = tracker.get("2R1G", 0) + 1
+        if red > green:
+            tracker["R>=G"] = tracker.get("R>=G", 0) + 1
+    return tracker
+
+
+simulate_draws(1000)
